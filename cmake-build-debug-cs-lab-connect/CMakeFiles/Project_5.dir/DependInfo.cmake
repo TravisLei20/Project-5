968a1e5e50178086f8a7d4a1f302a6a917be9a78
Travis <travisreynertson@gmail.com>
2021-07-27 14:32:13 -0600

    Here we go again

---
+++ cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/l/leicr/LiveProject-5/ColonAutomaton.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/ColonAutomaton.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/ColonDashAutomaton.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/ColonDashAutomaton.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/Lexer.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/Lexer.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/MatcherAutomaton.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/MatcherAutomaton.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/Parser.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/Parser.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/Token.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/Token.cpp.o"
+  "/users/guest/l/leicr/LiveProject-5/main.cpp" "/users/guest/l/leicr/LiveProject-5/cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-cs-lab-connect/CMakeFiles/Project_5.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_5.dir/ColonAutomaton.cpp.o"
+  "CMakeFiles/Project_5.dir/ColonDashAutomaton.cpp.o"
+  "CMakeFiles/Project_5.dir/Lexer.cpp.o"
+  "CMakeFiles/Project_5.dir/MatcherAutomaton.cpp.o"
+  "CMakeFiles/Project_5.dir/Parser.cpp.o"
+  "CMakeFiles/Project_5.dir/Token.cpp.o"
+  "CMakeFiles/Project_5.dir/main.cpp.o"
+  "Project_5"
+  "Project_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
